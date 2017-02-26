c128e9f506669d8da135b31330eea85615e36fe5
Stefan Hyltoft <stefanhyltoft@Stefans-Mac-mini.local>
2017-02-27 00:24:09 +0100

    Done

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,31 @@ import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
+import { Actions } from 'react-native-router-flux';
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
     return (
         <Router sceneStyle={{ paddingTop: 60 }}>
-            <Scene key="login" component={LoginForm} title="Please login" />
-            <Scene key="EmployeeList" component={EmployeeList} title="Employees" />
+            <Scene key="auth">
+                <Scene 
+                key="login" 
+                component={LoginForm} title="Please login" 
+                />
+            </Scene>
+
+            <Scene key="main">
+                <Scene 
+                rightTitle="Add"
+                onRight={() => Actions.employeeCreate()}
+                key="employeeList" 
+                component={EmployeeList} title="Employees" 
+                initial
+                />
+            </Scene>
+            <Scene key="employeeCreate" title="Create employee" component={EmployeeCreate} />
+            <Scene key="employeeEdit" title="Edit Employee" component={EmployeeEdit} />
         </Router>
     );
 };
